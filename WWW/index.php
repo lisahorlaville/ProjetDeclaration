e8230112d4179eb5987960b366635b20db2f4d4e
lavolsab <sabine.lavollee@etu.univ-rouen.fr>
2018-11-07 16:00:03 +0100

    Compléments fichiers standards
    Remplissage des fichiers .php et du style.scss basique

---
+++ WWW/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" >
+	<head>
+		<meta charset="UTF-8">
+		<title>DUDH | Accueil</title>
+		<link rel="icon" type="image/png" href="favicon.png" />
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+
+		
+	</body>
+</html>
