f59c189d9c67ed1f83efc60f03cd54befa277d86
lavolsab <sabine.lavollee@etu.univ-rouen.fr>
2018-11-07 16:00:03 +0100

    Ajouts pour ScaleDown
    CSS et Js en Assets/ et en standard pour l'effet ScaleDown de declaration.php
    Modification de declaration.php avec le code correspondant pour l'effet, et l'explicatif pour remplir le fichier

---
+++ WWW/declaration.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" >
+	<head>
+		<meta charset="UTF-8">
+		<title>DUDH | Déclaration complète</title>
+		<link rel="icon" type="image/png" href="favicon.png" />
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+
+
+		<?php 
+
+
+		?>
+
+
+	</body>
+</html>
