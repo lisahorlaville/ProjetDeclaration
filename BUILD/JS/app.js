e491dfc4c010f4d6a65803a111fa5cb716c48bb1
lavolsab <sabine.lavollee@etu.univ-rouen.fr>
2018-11-21 13:39:36 +0100

    Ajouts pour ScaleDown
    CSS et Js en Assets/ et en standard pour l'effet ScaleDown de declaration.php
    Modification de declaration.php avec le code correspondant pour l'effet, et l'explicatif pour remplir le fichier

---
+++ BUILD/JS/app.js
@@ -0,0 +1,4 @@
+//@prepros-prepend 'Assets/jquery-2.1.4.js';
+//@prepros-prepend 'Assets/main.js';
+//@prepros-prepend 'Assets/velocity.min.js';
+//@prepros-prepend 'Assets/velocity.ui.min.js',
--- WWW/declaration.php
+++ WWW/declaration.php
@@ -6,8 +6,17 @@
 		<link rel="icon" type="image/png" href="favicon.png" />
 		<link rel="stylesheet" href="css/style.css">
 	</head>
-	<body>
+	<body data-hijacking="on" data-animation="scaleDown" class='declaration'>
+<!-- Mettre ici en import les différents articles à la suite, avec dedans :
 
+	<section class="cd-section visible">
+		<div>
+			LE CONTENU
+		</div>
+	</section>
+
+Attention : Le class "visible" n'est à écrire que sur la toute première page, mettre juste "cd-section" pour les autres.
+-->
 
 		<?php 
 
