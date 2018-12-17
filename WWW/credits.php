cc50f4d1fdb123ccd5a64d3a3f4f382edc7c267f
claire <claire.langolff@hotmail.fr>
2018-12-17 21:57:16 +0100

    Mentions légales
    Fin de la mise en page des mentions légales

--- WWW/credits.php
+++ WWW/credits.php
@@ -17,17 +17,23 @@
 
 Attention : Le class "visible" n'est à écrire que sur la toute première page, mettre juste "cd-section" pour les autres.
  -->
- 	<h1>Mentions légales</h1>
+<div class="back">
+	<a href="index.php"><h3>Accueil</h3></a>
+	<a href="declaration.php"><h3>Déclaration</h3></a>
+</div>
+
+ <div class="mention_legale">
+ 	<h2>Mentions légales</h2>
  	<p>Projet Git, Déclaration des Droits de l'Homme<br/><br/>
 
-Ce projet de site internet a été réalisé dans le cadre d'un projet de groupe en Licence Professionnelle Métiers du Numérique à Elbeuf (76), Université Rouen-Normandie, promotion 2019.
-La DUDH appartient aux Nations Unies qui en sont rédacteurs.
-La mise en page et la charte graphique sont empruntées et inspirées de la version papier éditée par Amnesty International en 2018 à Paris, sous la direction artistique de l'agence be-poles.
+Ce projet de site internet a été réalisé dans le cadre d'un projet de groupe en Licence Professionnelle Métiers du Numérique à Elbeuf (76), Université Rouen-Normandie, promotion 2019.<br/>
+La DUDH appartient aux Nations Unies qui en sont rédacteurs.<br/>
+La mise en page et la charte graphique sont empruntées et inspirées de la version papier éditée par <a href="https://www.amnesty.fr/" target="blank">Amnesty International </a>en 2018 à Paris, sous la direction artistique de l'agence be-poles.<br/><br/>
 
-Enfin, à l'aide du plugin Javascript "Velocity", ce projet de livre intéractif est élaboré en PHP, HTML, CSS et SVG par Sabine Lavollée, Claire Langolff et Lisa Horlaville.
-
+Enfin, à l'aide du plugin Javascript "Velocity", ce projet de livre intéractif est élaboré en PHP, HTML, CSS et SVG par <a href="sabinelavollee.com" target="blank">Sabine Lavollée</a>, <a href="clairelangolff.fr" target="blank">Claire Langolff</a> et <a href="horlavillelisa.fr" target="blank">Lisa Horlaville</a>.
+<br/>
 Tous droits d'utilisation réservés.</p>
-
+</div>
 	<script src="js/app.js"></script>
 	</body>
 </html>
