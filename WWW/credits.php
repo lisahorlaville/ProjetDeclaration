57b9d3cbb0917997ae143148eab2a21397797699
lavolsab <sabine.lavollee@etu.univ-rouen.fr>
2018-12-17 20:17:50 +0100

    Mentions légales
    Fichier des mentions légales, lien ajouté sur declaration.php avec CSS fait pour la class du lien.
    Reste à faire le css de la page en elle-même et à ajouter le lien à la page d'accueil.

---
+++ WWW/credits.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr" >
+	<head>
+		<meta charset="UTF-8">
+		<title>DUDH | Crédit</title>
+		<link rel="icon" type="image/png" href="favicon.png" />
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+
+<!--
+	<section class="cd-section visible">
+		<div>
+			LE CONTENU
+		</div>
+	</section>
+
+Attention : Le class "visible" n'est à écrire que sur la toute première page, mettre juste "cd-section" pour les autres.
+ -->
+ 	<h1>Mentions légales</h1>
+ 	<p>Projet Git, Déclaration des Droits de l'Homme<br/><br/>
+
+Ce projet de site internet a été réalisé dans le cadre d'un projet de groupe en Licence Professionnelle Métiers du Numérique à Elbeuf (76), Université Rouen-Normandie, promotion 2019.
+La DUDH appartient aux Nations Unies qui en sont rédacteurs.
+La mise en page et la charte graphique sont empruntées et inspirées de la version papier éditée par Amnesty International en 2018 à Paris, sous la direction artistique de l'agence be-poles.
+
+Enfin, à l'aide du plugin Javascript "Velocity", ce projet de livre intéractif est élaboré en PHP, HTML, CSS et SVG par Sabine Lavollée, Claire Langolff et Lisa Horlaville.
+
+Tous droits d'utilisation réservés.</p>
+
+	<script src="js/app.js"></script>
+	</body>
+</html>
